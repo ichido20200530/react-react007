a0c1b500eb5eb78b0830d2518507fed686ff7045
ichido <ichido@live.jp>
2020-06-07 12:25:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import Rect from './Rect';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  data = [];
 
-export default App;
+  msgStyle = {
+    fontSize: "20pt",
+    color: "#900",
+    margin: "200px 0px",
+    padding: "5px",
+  }
+  area = {
+    width: "500px",
+    height: "500px",
+    border: "1px solid blue",
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list: this.data
+    };
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    this.data.push({x:x, y:y});
+    this.setState({
+      list: this.data
+    });
+  }
+
+  draw(d){
+    let s = {
+      position: "absolute",
+      left: (d.x - 25) + "px",
+      top: (d.y - 25) + "px",
+      width: "50px",
+      height: "50px",
+      backgroundColor: "#66f3",
+    };
+    return <div style={s}></div>;
+  }
+
+  render () {
+    return <div>
+      <h1>React</h1>
+      <h2 style={this.msgStyle}>show rect.</h2>
+      <div style={this.area} onClick={this.doAction}>
+        {this.data.map((value) => this.draw(value))}
+      </div>
+    </div>;
+  }
+}
